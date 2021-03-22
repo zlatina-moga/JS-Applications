659716c533ef4153fc51a81b9a88f9a48ed870c0
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-03-22 16:45:47 +0200

    Add files via upload

---
+++ Architecture and Testing/Lab/Accordion/test.js
@@ -0,0 +1,37 @@
+const {chromium} = require('playwright-chromium');
+const {expect} = require('chai');
+
+let browser, page;
+describe('E2E tests', function () {
+    before(async () => {
+        browser = await chromium.launch(); //{headless: false, slowMo: 500}
+    });
+
+    after(async () => {
+        await browser.close();
+    })
+
+    beforeEach(async () => {
+        page = await browser.newPage();
+    })
+
+    afterEach(async () => {
+        await page.close();
+    })
+
+    it('load titles', async () => {
+        await page.goto('http://localhost:3000');
+
+        const content = await page.textContent('.accordion .head span');
+        expect(content).to.contains('Scalable Vector Graphics')
+    })
+
+    it('toggles content', async () => {
+        await page.goto('http://localhost:3000');
+
+        await page.click('text=More');
+        await page.waitForSelector('.extra p')
+        const visible = await page.isVisible('.extra p');
+        expect(visible).to.be.true;
+    })
+});
