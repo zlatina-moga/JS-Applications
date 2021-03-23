7e57f8345b8befb99dcbd783a689f6ebc6dc837f
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-03-23 08:28:33 +0200

    Add files via upload

--- Architecture and Testing/Lab/Accordion/test.js
+++ Architecture and Testing/Lab/Accordion/test.js
@@ -18,26 +18,30 @@ describe('E2E tests', function () {
 
     it('load titles', async () => {
         await page.goto('http://localhost:3000');
+        /*const content = await page.textContent('.accordion .head span');
+        expect(content).to.contains('Scalable Vector Graphics')*/
 
-        const content = await page.textContent('.accordion .head span');
-        expect(content).to.contains('Scalable Vector Graphics')
+        const titles = await page.$$eval('.accordion .head span', (spans) => spans.map(s => s.textContent));
+        expect(titles).includes('Scalable Vector Graphics')
+        expect(titles).includes('Open standard')
+        expect(titles).includes('Unix')
+        expect(titles).includes('ALGOL')
     })
 
     it('toggles content', async () => {
         await page.goto('http://localhost:3000');
-
         await page.click('#main>.accordion:first-child >> text=More');
         await page.waitForSelector('#main>.accordion:first-child >> .extra p')
         const visible = await page.isVisible('#main>.accordion:first-child >> .extra p');
         expect(visible).to.be.true;
     })
 
-    it('toggles content', async () => {
+    it('button functionality', async () => { //.only
         await page.goto('http://localhost:3000');
-
         await page.click('#main>.accordion:first-child >> text=More');
         await page.waitForSelector('#main>.accordion:first-child >> .extra p')
+        await page.click('#main>.accordion:first-child >> text=Less');
         const visible = await page.isVisible('#main>.accordion:first-child >> .extra p');
-        expect(visible).to.be.true;
+        expect(visible).to.be.false;
     })
 });
