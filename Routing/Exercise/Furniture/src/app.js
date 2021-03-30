728519a96b0ccee477c2fda9e88c9f1386676fa2
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-03-30 15:35:27 +0300

    Add files via upload

--- Routing/Exercise/Furniture/src/app.js
+++ Routing/Exercise/Furniture/src/app.js
@@ -1,4 +1,7 @@
 import page from '../node_modules/page/page.mjs';
+import {render} from 'https://unpkg.com/lit-html?module';
+
+import {logout} from './api/data.js'
 import {createPage} from './views/create.js';
 import { dashboardPage } from './views/dashboard.js';
 import { detailsPage } from './views/details.js';
@@ -7,15 +10,38 @@ import { loginPage } from './views/login.js'
 import { myPage } from './views/myFurniture.js'
 import { registerPage } from './views/register.js';
 
-import * as api from './api/data.js';
-window.api = api;
+const main = document.querySelector('.container')
 
-page('/', dashboardPage);
-page('/dashboard', dashboardPage);
-page('details/:id', detailsPage);
-page('/create', createPage);
-page('/edit/:id', editPage);
-page('/register', registerPage);
-page('/login', loginPage);
-page('/my-furniture', myPage);
+page('/', decorateContext, dashboardPage);
+page('/details/:id', decorateContext, detailsPage);
+page('/create', decorateContext, createPage);
+page('/edit/:id', decorateContext, editPage);
+page('/register', decorateContext, registerPage);
+page('/login', decorateContext, loginPage);
+page('/my-furniture', decorateContext, myPage);
+
+document.getElementById('logoutBtn').addEventListener('click', async () => {
+    await logout();
+    setUserNav()
+    page.redirect('/')
+})
+
+setUserNav()
 page.start()
+
+function decorateContext(ctx, next){
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+    next()
+}
+
+function setUserNav(){
+    const userId = sessionStorage.getItem('userId')
+    if (userId != null){
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
