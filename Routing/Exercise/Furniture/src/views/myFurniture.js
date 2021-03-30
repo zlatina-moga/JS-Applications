d4ce4de967df8d88430301bb061fe4afb860410d
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-03-30 15:36:24 +0300

    Add files via upload

--- Routing/Exercise/Furniture/src/views/myFurniture.js
+++ Routing/Exercise/Furniture/src/views/myFurniture.js
@@ -1,3 +1,19 @@
-export async  function myFurniture(ctx){
-    console.log('furniture')
+import {html} from 'https://unpkg.com/lit-html?module';
+import {getMyFurniture} from '../api/data.js';
+import {itemTemplate} from './common/item.js'
+
+const myTemplate = (data) => html `
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>My Furniture</h1>
+        <p>This is a list of your publications.</p>
+    </div>
+</div>
+<div class="row space-top">
+    ${data.map(itemTemplate)}
+</div>`;
+
+export async function myPage(ctx){
+    const data = await getMyFurniture();
+    ctx.render(myTemplate(data))
 }
