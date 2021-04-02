c24048ed07cff81f6cf7cd4b72f00bf875f11218
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-04-02 14:11:18 +0300

    Update catalog.js

--- Exams/26.03.21 - Meme Lounge/src/views/catalog.js
+++ Exams/26.03.21 - Meme Lounge/src/views/catalog.js
@@ -16,7 +16,7 @@ const memeTemplate = (meme) => html`
     <div class="card">
         <div class="info">
             <p class="meme-title">${meme.title}</p>
-            <img class="meme-image" alt="meme-img" src=${meme.imageUrl}>
+            <img class="meme-image" alt="meme-img" src=${meme.imageUrl} >
         </div>
         <div id="data-buttons">
             <a class="button" href="/details/${meme._id}">Details</a>
