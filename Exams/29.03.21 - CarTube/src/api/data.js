9f33d7b9285fb2a72191ab57a8afea6b8323266e
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-04-03 16:00:36 +0300

    Add files via upload

---
+++ Exams/29.03.21 - CarTube/src/api/data.js
@@ -0,0 +1,31 @@
+import * as api from './api/api.js';
+const host = 'http://localhost:3030'
+api.settings.host = host;
+
+export const login = api.login;
+export const logout = api.logout;
+export const register = api.register;
+
+export async function getAllListings(){
+    return await api.get(host + '/data/cars?sortBy=_createdOn%20desc')
+}
+
+export async function getListingById(id){
+    return await api.get(host + '/data/cars/' + id)
+}
+
+export async function createListing(listing){
+    return await api.post(host + '/data/cars', listing)
+}
+
+export async function updateListing(id, listing){
+    return await api.put(host + '/data/cars/' + id, listing)
+}
+
+export async function deleteListing(id){
+    return await api.del(host + '/data/cars/' + id)
+}
+
+export async function getMyListings(userId){
+    return await api.get(host + `/data/cars?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`)
+}
---
+++ Exams/29.03.21 - CarTube/src/app.js
@@ -0,0 +1,2 @@
+import * as api from './api/data.js';
+window.api = api;
